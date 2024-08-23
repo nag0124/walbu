8ee60af7738b5e01ddf462dacda7280351d0322a
nag0124 <wlsdnjs0124@gmail.com>
2024-08-23 15:38:08 +0900

    test: 동일 이름으로 회원 가입시 실패하는 테스트 구현  #2
    - 통합 테스트에서 빼먹은 테스트가 있어 추가합니다.

--- src/test/java/walbu/project/domain/member/MemberIntegrationTest.java
+++ src/test/java/walbu/project/domain/member/MemberIntegrationTest.java
@@ -4,14 +4,22 @@ import static org.hamcrest.Matchers.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import walbu.project.IntegrationTest;
+import walbu.project.common.error.exception.SameNameMemberExistsException;
+import walbu.project.domain.member.data.Member;
 import walbu.project.domain.member.data.MemberType;
 import walbu.project.domain.member.data.dto.CreateMemberRequest;
+import walbu.project.domain.member.repository.MemberRepository;
 
 public class MemberIntegrationTest extends IntegrationTest {
+
+
+    @Autowired
+    private MemberRepository memberRepository;
 
     @Test
     @DisplayName("회원 가입한다.")
@@ -37,6 +45,41 @@ public class MemberIntegrationTest extends IntegrationTest {
                 .statusCode(200)
                 .body("memberId", notNullValue());
 
+    }
+
+    @Test
+    @DisplayName("동일한 이름의 회원이 가입하려고 하면 에러가 발생한다.")
+    void sameNameCantSinUp() {
+        // given
+        Member member = new Member(
+                "nag",
+                "nag@walbu.com",
+                "1q2w3e4r!",
+                "01012341234",
+                MemberType.STUDENT
+        ) ;
+        memberRepository.save(member);
+
+        CreateMemberRequest request = new CreateMemberRequest(
+                member.getName(),
+                "asdf@walbu.com",
+                "1q2w3e4r!",
+                "01012341234",
+                MemberType.STUDENT
+        );
+        SameNameMemberExistsException exception = new SameNameMemberExistsException();
+
+        // when & then
+        RestAssured
+                .given().log().all()
+                .contentType(ContentType.JSON)
+                .accept(ContentType.JSON)
+                .body(request)
+                .when()
+                .post("/api/members")
+                .then().log().all()
+                .statusCode(400)
+                .body("message", equalTo(exception.getMessage()));
     }
 
 }
