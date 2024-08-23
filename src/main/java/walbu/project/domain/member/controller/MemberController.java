1ebc2fbdbc0aea3bc09e114ddbbf5336b9bbd456
nag0124 <wlsdnjs0124@gmail.com>
2024-08-23 11:23:26 +0900

    feat: MemberController 생성 & 회원 가입 로직 구현 #2

---
+++ src/main/java/walbu/project/domain/member/controller/MemberController.java
@@ -0,0 +1,30 @@
+package walbu.project.domain.member.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import walbu.project.domain.member.data.dto.CreateMemberRequest;
+import walbu.project.domain.member.data.dto.CreateMemberResponse;
+import walbu.project.domain.member.service.MemberService;
+
+@RestController
+@RequestMapping("/api/members")
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @PostMapping
+    public ResponseEntity<CreateMemberResponse> createMember(@RequestBody CreateMemberRequest request) {
+        CreateMemberResponse response = memberService.createMember(request);
+
+        return ResponseEntity
+                .ok()
+                .body(response);
+    }
+
+}
