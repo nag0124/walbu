c9258f736c2108cc254540030391b604d6e23547
nag0124 <wlsdnjs0124@gmail.com>
2024-08-23 18:21:01 +0900

    test: 강의 개설 Request 필드 유효성 검증 테스트 작성 #3

--- src/test/java/walbu/project/domain/lecture/LectureControllerTest.java
+++ src/test/java/walbu/project/domain/lecture/LectureControllerTest.java
@@ -51,4 +51,172 @@ public class LectureControllerTest {
                 .andExpect(status().isOk());
     }
 
+    @Test
+    @DisplayName("강사 아이디는 1보다 작을 수 없다.")
+    void requestInstructorIdIsOver0() throws Exception {
+        // given
+        CreateLectureRequest request = new CreateLectureRequest(
+                0L,
+                "나그와 함께하는 부동산",
+                10000,
+                10
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/lectures")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("강사 아이디는 null일 수 없다.")
+    void requestInstructorIdIsNotNull() throws Exception {
+        // given
+        CreateLectureRequest request = new CreateLectureRequest(
+                null,
+                "나그와 함께하는 부동산",
+                10000,
+                10
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/lectures")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("강의 이름은 빈칸일 수 없다.")
+    void requestNameIsNotBlank() throws Exception {
+        // given
+        CreateLectureRequest request = new CreateLectureRequest(
+                1L,
+                " ",
+                10000,
+                10
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/lectures")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("수강 인원은 1보다 작을 수 없다.")
+    void requestEnrollmentCountIsOver0() throws Exception {
+        // given
+        CreateLectureRequest request = new CreateLectureRequest(
+                1L,
+                "나그와 함께하는 부동산",
+                10000,
+                0
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/lectures")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("수강 인원은 null일 수 없다.")
+    void requestEnrollmentCountIsNotNull() throws Exception {
+        // given
+        CreateLectureRequest request = new CreateLectureRequest(
+                1L,
+                "나그와 함께하는 부동산",
+                10000,
+                null
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/lectures")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("강의 가격은 0일 수 있다.")
+    void requestPriceCanBeZero() throws Exception {
+        // given
+        CreateLectureRequest request = new CreateLectureRequest(
+                1L,
+                "나그와 함께하는 부동산",
+                0,
+                10
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/lectures")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("강의 가격은 음수일 수 없다.")
+    void requestPriceIsNotNegative() throws Exception {
+        // given
+        CreateLectureRequest request = new CreateLectureRequest(
+                1L,
+                "나그와 함께하는 부동산",
+                -1,
+                10
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/lectures")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("강의 가격은 null일 수 없다.")
+    void requestPriceIsNotNull() throws Exception {
+        // given
+        CreateLectureRequest request = new CreateLectureRequest(
+                1L,
+                "나그와 함께하는 부동산",
+                null,
+                10
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/lectures")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
 }
