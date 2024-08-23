61664196e34b4cc5271cbf98f5f2ccc3e5e0b917
nag0124 <wlsdnjs0124@gmail.com>
2024-08-23 17:34:40 +0900

    feat: LectureController 생성 & Lecture 생성 기능 구현 #3

---
+++ src/main/java/walbu/project/domain/lecture/controller/LectureController.java
@@ -0,0 +1,31 @@
+package walbu.project.domain.lecture.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+import walbu.project.domain.lecture.data.dto.CreateLectureRequest;
+import walbu.project.domain.lecture.data.dto.CreateLectureResponse;
+import walbu.project.domain.lecture.service.LectureService;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/lectures")
+public class LectureController {
+
+    private final LectureService lectureService;
+
+    @PostMapping
+    public ResponseEntity<CreateLectureResponse> createLecture(@RequestBody CreateLectureRequest request) {
+        CreateLectureResponse response = lectureService.createLecture(request);
+
+        return ResponseEntity
+                .ok()
+                .body(response);
+    }
+
+}
