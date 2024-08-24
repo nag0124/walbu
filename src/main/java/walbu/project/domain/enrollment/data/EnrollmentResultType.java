44b0dd19538adc815b5852f48f359c1e8e3bd9a9
nag0124 <wlsdnjs0124@gmail.com>
2024-08-24 14:05:49 +0900

    refactor: 수강 등록 실패시 HttpStatus 변경 #4
    - 404 BAD_REQUEST에서 409 CONFLICT로 변경해 의미가 잘 드러나도록 합니다.

--- src/main/java/walbu/project/domain/enrollment/data/EnrollmentResultType.java
+++ src/main/java/walbu/project/domain/enrollment/data/EnrollmentResultType.java
@@ -10,7 +10,7 @@ import lombok.RequiredArgsConstructor;
 public enum EnrollmentResultType {
 
     SUCCESS(HttpStatus.OK, "수강 신청에 성공했습니다."),
-    FAIL(HttpStatus.BAD_REQUEST, "수강 신청에 실패했습니다.");
+    FAIL(HttpStatus.CONFLICT, "수강 신청에 실패했습니다.");
 
     private final HttpStatus status;
     private final String message;
