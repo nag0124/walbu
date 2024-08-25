59d5d2f4acada2320538f6d8cf8a24558b799c11
nag0124 <wlsdnjs0124@gmail.com>
2024-08-25 16:36:17 +0900

    test: 기본적인 CreateMemberRequest 유효성 테스트 #7

--- src/test/java/walbu/project/domain/member/MemberControllerTest.java
+++ src/test/java/walbu/project/domain/member/MemberControllerTest.java
@@ -4,6 +4,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import java.util.Map;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -51,6 +53,94 @@ public class MemberControllerTest {
                 )
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("회원 가입 Request의 이름 유효성 검증에 실패하면 400 응답을 보낸다.")
+    void createInvalidNameMember() throws Exception {
+        // given
+        CreateMemberRequest request = new CreateMemberRequest(
+                "",
+                "nag@walbu.com",
+                "1q2w3e4r!",
+                "01012341234",
+                MemberType.STUDENT
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/members")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("회원 가입 Request의 이메일 유효성 검증에 실패하면 400 응답을 보낸다.")
+    void createInvalidEmailMember() throws Exception {
+        // given
+        CreateMemberRequest request = new CreateMemberRequest(
+                "nag",
+                "nag",
+                "1q2w3e4r!",
+                "01012341234",
+                MemberType.STUDENT
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/members")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("회원 가입 Request의 핸드폰 번호 유효성 검증에 실패하면 400 응답을 보낸다.")
+    void createInvalidPhoneNumberMember() throws Exception {
+        // given
+        CreateMemberRequest request = new CreateMemberRequest(
+                "nag",
+                "nag@walbu.com",
+                "1q2w3e4r!",
+                "0",
+                MemberType.STUDENT
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/members")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    @DisplayName("회원 가입 Request의 회원 타입 유효성 검증에 실패하면 400 응답을 보낸다.")
+    void createInvalidTypeMember() throws Exception {
+        // given
+        Map<String, String> request = Map.of(
+                "name", "nage",
+                "email", "nag@walbu.com",
+                "password", "1q2w3e4r!",
+                "phoneNumber", "01012341234",
+                "type", "admin"
+        );
+
+        // when & then
+        mockMvc.perform(
+                        post("/api/members")
+                                .content(objectMapper.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON)
+                )
+                .andDo(print())
+                .andExpect(status().isBadRequest());
     }
 
 }
